469509ce1d329c6c24f1987513f3e77ac5fab195
Leon Da Costa <leon@dacosta.me>
2015-11-04 23:34:59 +0000

    Update 0.1.2

--- js/form-overlay.js
+++ js/form-overlay.js
@@ -9,16 +9,29 @@
  * Version: 0.1
  */
 
-
 var FormOverlay = function FormOverlay(){
 	
 	var settings = {
   		tnId: null,
   		formContainer: null,
   		postUrl: null
-}
+	}
 	
 	FormOverlay.prototype.count = 4;
+	
 	console.log(arguments.length);
 	console.log(arguments[0]);
+	var json = arguments[0];
+	console.log(json.btnId);
+	
+	var btnId = json.btnId;
+	var btn = document.getElementById(btnId);
+	
+	console.log(btnId);
+	
+	window.onload = function() {
+    	document.getElementById(btnId).onclick = function btnClick() {
+        	alert("Simple JS From Overlay");
+    	}
+	}
 }
