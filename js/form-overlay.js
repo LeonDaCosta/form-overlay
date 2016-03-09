37e33e5eb8739ff47ce4d5c5bd4198c37eb2415f
Leon Da Costa <leon@onbrandgroup.co.uk>
2016-03-09 15:34:34 +0000

    Simply correction.

--- js/form-overlay.js
+++ js/form-overlay.js
@@ -10,55 +10,55 @@
  */
 
 var FormOverlay = function FormOverlay(){
-	
+
 	var settings = {
-  		tnId: null,
+  		btnId: null,
   		formContainer: null,
   		postUrl: null
 	}
-	
+
 	FormOverlay.prototype.count = 4;
-	
+
 	var json = arguments[0];
 	var btnId = json.btnId;
 	var formDiv = json.formContainer;
-	
+
 	var btn = document.getElementById(btnId);
-	
+
 	window.onload = function() {
 		document.body.innerHTML += '<div id="overlay" class="overlay-hide"></div>';
     	document.getElementById(btnId).onclick = function btnClick() {
 			 addBackground();
     	}
-    	
+
     	document.getElementById('overlay').onclick = function btnClick() {
 			 removeBackground();
     	}
-    	
+
     	function removeBackground() {
     		document.getElementById('overlay').className = "";
     		removeForm();
     	}
-    	
+
     	function addBackground() {
     		document.getElementById('overlay').className = "";
     		document.getElementById('overlay').className += 'overlay';
     		addForm();
     	}
-    	
+
     	function addForm() {
     		var formc = document.getElementById(formDiv);
     		formc.style.display = 'block';
     		document.getElementById(formDiv).className += "overlay-window overlay-form-div";
     	}
-    	
+
     	function removeForm() {
     		var formc = document.getElementById(formDiv);
     		formc.className = "";
     		formc.style.display = 'none';
     	}
-    	
+
     	var formc = document.getElementById(formDiv);
     	formc.style.display = 'none';
-	}	
+	}
 }
